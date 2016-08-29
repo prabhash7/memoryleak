192b0844d0a6da2674239a922001bff1c22e0cfa
freemb2 <brian.freeman@emc.com>
2016-08-29 15:00:03 -0600

    Update provider controller

--- server/src/main/java/com/emc/memoryleaks/controllers/ProviderController.java
+++ server/src/main/java/com/emc/memoryleaks/controllers/ProviderController.java
@@ -1,70 +1,38 @@
 package com.emc.memoryleaks.controllers;
 
-import com.emc.edp4vcac.domain.EdpFeature;
-import com.emc.edp4vcac.domain.EdpNotificationHandler;
-import com.emc.edp4vcac.domain.EdpRepository;
 import com.emc.edp4vcac.domain.EdpSystem;
-import com.emc.edp4vcac.domain.spi.DataProtectionProviderConfig;
+import com.emc.memoryleaks.service.RepositoryService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
 
 @RestController
 public class ProviderController {
 
     private static final Logger logger = LoggerFactory.getLogger(ProviderController.class);
 
-    private final EdpRepository edpRepository;
+    @Autowired
+    private RepositoryService repoSvc;
 
-    ProviderController() {
+    public ProviderController() {
         logger.debug("Starting ProviderController");
-
-        EdpNotificationHandler notificationHandler = new EdpNotificationHandler() {
-            @Override
-            public void notifyElementUpdated(EntityType entityType, String s) {
-
-            }
-
-            @Override
-            public void notifyElementDeleted(EntityType entityType, String s) {
-
-            }
-
-            @Override
-            public void notifyElementInvalidate(EntityType entityType, String s) {
-
-            }
-        };
-
-        Map<EdpFeature,Boolean> licensedFeatures = new HashMap<>();
-
-        // TODO: need to determine how to shutdown cleanly
-        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
-
-        edpRepository = new EdpRepository(notificationHandler, licensedFeatures, executorService);
-
-//        DataProtectionProviderConfig dpProviderConfig = new DataProtectionProviderConfig()
-//        edpRepository.addSystem("daves-avamar", dpProviderConfig, true);
     }
 
     @CrossOrigin(origins = "*")
     @RequestMapping("/provider")
     public List<EdpSystem> getProviderList() {
-        logger.debug("/providers request");
-        return edpRepository.findAllSystems();
+        logger.debug("get /providers");
+        return repoSvc.findAllSystems();
     }
 
-    @PostMapping("/provider")
-    public EdpSystem createProvider() {
-        return null;
-    }
+//    @PostMapping("/provider")
+//    public EdpSystem createProvider() {
+//        logger.debug("post /provider ");
+//        return null;
+//    }
 }
