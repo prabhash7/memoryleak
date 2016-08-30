e22df156e54e2a9159f955028230df85f547fbd9
Michael Brown <michaelbrown@Michaels-MacBook-Pro.local>
2016-08-30 12:12:37 -0600

    More UI changes.

--- server/src/main/resources/static/app/app.js
+++ server/src/main/resources/static/app/app.js
@@ -40,8 +40,23 @@ angular.module('myApp').directive('policyDetails', function () {
     };
 });
 
+angular.module('myApp').directive('clientList', function () {
+    return {
+        restrict: 'E',
+        templateUrl: "clientList.html"
+    };
+});
+
+angular.module('myApp').directive('clientDetails', function () {
+    return {
+        restrict: 'E',
+        templateUrl: "clientDetails.html"
+    };
+});
+
 angular.module('myApp').controller('memoryLeaksCtrl', function ($scope, $http) {
-    $http.get('/policies').
+    $scope.selectedTab = 'clients';
+	$http.get('/policies').
         success(function (data) {
             $scope.policies = data;
             console.log('received: ' + $scope.policies.id);
@@ -56,11 +71,17 @@ angular.module('myApp').controller('memoryLeaksCtrl', function ($scope, $http) {
                 $scope.provider = data[0].name;
             }
             console.log('received: ' + $scope.provider);
+            var providerId = data[0].id;
+            $http.get('/provider/' + providerId + '/client').
+                success(function (data) {
+                    $scope.clients = data;
+            });
         });
     $scope.setSelectedPolicy = function (policy) {
         $scope.selectedPolicy = policy;
     };
 });
+
 
 angular.module('myApp').config(function (treeConfig) {
     treeConfig.defaultCollapsed = true;
