b7b693a92927ba3a6e342a2f04596da6194ad3d1
Mike Schmidt <michael.schmidt@emc.com>
2016-08-30 10:38:23 -0600

    Updates to support baseBean

--- server/src/main/java/com/emc/memoryleaks/beans/Client.java
+++ server/src/main/java/com/emc/memoryleaks/beans/Client.java
@@ -1,5 +1,7 @@
 package com.emc.memoryleaks.beans;
 
+import com.emc.edp4vcac.domain.EdpClient;
+
 /**
  * Created by freemb2 on 8/30/16.
  */
@@ -7,5 +9,13 @@ public class Client extends BaseBean {
 
     public Client(String id, String name, String description, String details) {
         super(id, name, description, details);
+    }
+
+    public static Client convert(final EdpClient c) {
+        if (c != null) {
+            return new Client(c.getId(), c.getDisplayName(), c.getDescription(), "");
+        } else {
+            return null;
+        }
     }
 }
--- server/src/main/java/com/emc/memoryleaks/beans/Provider.java
+++ server/src/main/java/com/emc/memoryleaks/beans/Provider.java
@@ -1,5 +1,7 @@
 package com.emc.memoryleaks.beans;
 
+import com.emc.edp4vcac.domain.EdpSystem;
+
 /**
  * Created by freemb2 on 8/29/16.
  */
@@ -7,5 +9,13 @@ public class Provider extends BaseBean {
 
     public Provider(String id, String name, String description, String details) {
         super(id, name, description, details);
+    }
+
+    public static Provider convert(final EdpSystem edpSystem) {
+        if (edpSystem != null) {
+            return new Provider(edpSystem.getId(), edpSystem.getDisplayName(), edpSystem.getDescription(), "");
+        } else {
+            return null;
+        }
     }
 }
